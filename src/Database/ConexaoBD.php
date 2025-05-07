87d19916ba35168e5b014049b6159dd4f3105e0b
alanasrocha08 <alanasr02@gmail.com>
2025-05-07 08:32:40 -0300

    Update ConexaoBD.php

--- src/Database/ConexaoBD.php
+++ src/Database/ConexaoBD.php
@@ -1,10 +1,10 @@
-<?php
+<?php 
 namespace ExemploCrud;
 
-use PDO;
-use Throwable;
+use Exception;
+use PDO, Throwable;
 
-abstract class ConexaoBD
+abstract class ConexaoBD 
 {
     private static PDO $conexao;
     private static string $servidor = "localhost";
@@ -12,26 +12,26 @@ abstract class ConexaoBD
     private static string $senha = "";
     private static string $banco = "vendas";
 
-    public static function getConexao():PDO
+    public static function getConexao():PDO 
     {
-        if(!isset( self::$conexao)){
+        if( !isset(self::$conexao) ){
             try {
-            self::$conexao = new PDO(
-                "mysql:host=".self::$servidor.";dbname=".self::$banco.";charset=utf8",
-                self::$usuario, self::$senha 
-            );
+                
+                self::$conexao = new PDO(
+                    "mysql:host=".self::$servidor.";dbname=".self::$banco.";charset=utf8",
+                    self::$usuario, self::$senha
+                );
             
-            self::$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
+                
+                self::$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (Throwable $erro) {
-                //[A FAZER] Registra a exeção em um arquivo de log/texto interno
+                // [A FAZER] Registra a exceção em um arquivo de log/texto interno
 
                 // Lançar uma mensagem de erro genérica sem detalhes do banco/sistema
-                throw new Exception("Erro ao conectar com o banco de dados! ");
-            } //final do catch
-        } // dinal do if
-        
-        return self::$conexao;
-    }// final do getConexao
+                throw new Exception("Erro ao conectar com o banco de dados!");
+            }
+        }
 
+        return self::$conexao;
+    }
 }
